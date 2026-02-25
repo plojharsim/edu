e7f3eab9e90a3d0e2c3ee8b700a604e9d1cec407
[dyad] <plojharsim@gmail.com>
2026-02-25 18:56:43 +0100

    Reverted all changes back to version bd0a9f2dabde005b533b8f637dc3badb85463f92

--- src/data/predefined/index.ts
+++ src/data/predefined/index.ts
@@ -1,6 +1,7 @@
 import { Category } from "../studyData";
 import { anglictina } from "./anglictina";
 import { nemcina } from "./nemcina";
+import { zemepis } from "./zemepis";
 import { matematika } from "./matematika";
 import { chemie } from "./chemie";
 
@@ -9,6 +10,7 @@ import { chemie } from "./chemie";
 export const PREDEFINED_DATA: Record<string, Category> = {
   anglictina,
   nemcina,
+  zemepis,
   matematika,
   chemie
 };
