dfa81e15d98229d30797bb58010467317219d27a
[dyad] <plojharsim@gmail.com>
2026-02-25 19:00:20 +0100

    [dyad] Odstranění kategorií Zeměpis a Chemie z předdefinovaných dat. - wrote 1 file(s), deleted 2 file(s)

--- src/data/predefined/index.ts
+++ src/data/predefined/index.ts
@@ -1,16 +1,12 @@
 import { Category } from "../studyData";
 import { anglictina } from "./anglictina";
 import { nemcina } from "./nemcina";
-import { zemepis } from "./zemepis";
 import { matematika } from "./matematika";
-import { chemie } from "./chemie";
 
 // Chcete-li přidat novou sadu, vytvořte soubor ve složce predefined,
 // naimportujte ho sem a přidejte do objektu PREDEFINED_DATA.
 export const PREDEFINED_DATA: Record<string, Category> = {
   english: anglictina,
   german: nemcina,
-  geography: zemepis,
-  math: matematika,
-  chemistry: chemie
+  math: matematika
 };
